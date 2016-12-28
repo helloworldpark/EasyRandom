514ebb7c0b71d9966546bd2f9de9fc71dce06013
Sunghyun Park <neick123@linecorp.com>
2016-12-28 21:55:20 +0900

    Initial Commit

---
+++ EasyRandom/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EasyRandom
+//
+//  Created by LinePlus on 2016. 12. 28..
+//  Copyright © 2016년 Helloworld Park. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
