2089f64b310717df6735392ee76accea6bc8511a
Helloworld Park <helloworld.park@gmail.com>
2016-12-31 11:25:13 +0900

    ERDiscrete Complete

--- ERDiscrete.swift
+++ ERDiscrete.swift
@@ -9,19 +9,34 @@
 import Foundation
 
 public class ERDiscrete : RandomVariable {
-    let histogram: [Coord2D]
+    let cumulated: [Coord2D]
     
     init(histogram: [Coord2D]) {
         precondition(histogram.count != 0, "Empty Histogram!")
-        self.histogram = histogram
+        let sum = histogram.reduce(0.0) { result, coord in
+            return result + coord.y
+        }
+        var cumulative = [Coord2D](repeating: Coord2D(x: 0.0, y: 0.0), count: histogram.count+1)
+        var cumsum = 0.0
+        for i in 0..<histogram.count {
+            cumulative[i] = Coord2D(x: histogram[i].x, y: cumsum / sum)
+            cumsum += histogram[i].y
+        }
+        // Dummy
+        cumulative[cumulative.count-1] = Coord2D(x: histogram.last!.x + 1.0, y: 1.0)
+        self.cumulated = cumulative
     }
     
     func generate() -> Double {
         let u = Double(arc4random())/Double(UINT32_MAX)
-        return u
+        return self.cumulated.xrange(y: u)!.x1
     }
     
     func generate(count: Int) -> [Double] {
-        return [0.0]
+        var arr = [Double](repeating: 0.0, count: count)
+        for i in 0..<arr.count {
+            arr[i] = self.generate()
+        }
+        return arr
     }
 }
