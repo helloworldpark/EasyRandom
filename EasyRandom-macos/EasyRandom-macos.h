894446a75a306d56ca035e62f941d8d36c618f2e
Helloworld Park <helloworld.park@gmail.com>
2017-01-01 16:22:54 +0900

    Cocoa pod specification and support for iOS, macOS

---
+++ EasyRandom-macos/EasyRandom-macos.h
@@ -0,0 +1,19 @@
+//
+//  EasyRandom-macos.h
+//  EasyRandom-macos
+//
+//  Created by LinePlus on 2017. 1. 1..
+//  Copyright © 2017년 Helloworld Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyRandom-macos.
+FOUNDATION_EXPORT double EasyRandom_macosVersionNumber;
+
+//! Project version string for EasyRandom-macos.
+FOUNDATION_EXPORT const unsigned char EasyRandom_macosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyRandom_macos/PublicHeader.h>
+
+
---
+++ EasyRandom/EasyRandom.h
@@ -0,0 +1,19 @@
+//
+//  EasyRandom.h
+//  EasyRandom
+//
+//  Created by LinePlus on 2017. 1. 1..
+//  Copyright © 2017년 Helloworld Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EasyRandom.
+FOUNDATION_EXPORT double EasyRandomVersionNumber;
+
+//! Project version string for EasyRandom.
+FOUNDATION_EXPORT const unsigned char EasyRandomVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyRandom/PublicHeader.h>
+
+
